7c1a8826b5a846573e638720dcecf036769214ad
taeyoon0620 <woon0426@gmail.com>
2025-01-17 16:19:21 +0900

    1

--- src/pages/common/google_login.js
+++ src/pages/common/google_login.js
@@ -1,12 +1,8 @@
 import React from "react";
 
-const GoogleLoginComponent = () => {
+const GoogleLogin = () => {
   const handleGoogleLogin = () => {
-    const clientId = '800620068616-3ghg401p0pmbv8h1hj84qlj45qmkpgli.apps.googleusercontent.com'; // 구글 클라이언트 ID
-    const redirectUri = 'http://localhost:3000/google/success';
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid%20profile%20email`;
-
-    window.location.href = authUrl; // 구글 로그인 페이지로 리디렉션
+    window.location.href = 'http://localhost:8080/auth/login/google'; // 구글 로그인 요청 URL
   };
 
   return (
@@ -17,4 +13,4 @@ const GoogleLoginComponent = () => {
   );
 };
 
-export default GoogleLoginComponent;
+export default GoogleLogin;
