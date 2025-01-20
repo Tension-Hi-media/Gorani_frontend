0972a7970f0ede396c093d33dc1410e082533eb1
lucian1120000 <lucian1120@naver.com>
2025-01-20 12:33:42 +0900

    name fixed, login modal fied

--- src/pages/common/Footer.jsx
+++ src/pages/common/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./../../assets/css/all.css"; 
-import "./../../assets/css/Common/Footer.css";
+import "./../../assets/css/Common/footer.css";
 
 const Footer = () => {
   return (
--- src/pages/common/Modal.jsx
+++ src/pages/common/Modal.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../../assets/css/all.css";
-import "../../assets/css/Common/Modal.css";
+import "../../assets/css/Common/modal.css";
 import GoogleLoginComponent from "../Login/GoogleLogin";
-import NaverLogin from "../Login/Naver"; // NaverLogin 컴포넌트 import
-import KakaoLogin from "../Login/KakaoLogin"; // KakaoLogin 컴포넌트 import
+import NaverLogin from "../Login/Naver";
+import KakaoLogin from "../Login/KakaoLogin";
 
 const Modal = ({ isOpen, toggleModal }) => {
   if (!isOpen) return null; // 모달이 열리지 않았으면 아무것도 렌더링하지 않음
@@ -22,20 +22,9 @@ const Modal = ({ isOpen, toggleModal }) => {
         </button>
         <h2>로그인</h2>
 
-        <button className="login-button google">
-          <img className="logo" src="../../../images/google.png" alt="Google" />
-          Google 계정으로 로그인
-        </button>
-
-        <button className="login-button kakao">
-          <img className="logo" src="../../../images/kakao.jpg" alt="Kakao" />
-          Kakao 계정으로 로그인
-        </button>
-
-        <button className="login-button naver">
-          <img className="logo" src="../../../images/naver.png" alt="Naver" />
-          Naver 계정으로 로그인
-        </button>
+        <GoogleLoginComponent />
+        <KakaoLogin />
+        <NaverLogin />
       </div>
     </div>
   );
