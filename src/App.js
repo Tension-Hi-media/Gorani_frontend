38c3e749c1ec6dd74c4d954985c32243670b8510
taeyoon0620 <woon0426@gmail.com>
2025-01-21 17:13:06 +0900

    [fix] import 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route,} from 'react-router-dom';
-import Main from './pages/Main/Main';
+import Main from './pages/Main/main';
 import Translation from './pages/Translation/Translation';
 import NaverLogin from './pages/Login/Naver';
-import MyPage from './pages/User/MyPage';
+import MyPage from './pages/User/myPage';
 import NaverSuccess from './pages/Login/NaverSuccess';
 import KakaoLogin from './pages/Login/KakaoLogin';
 import KakaoSuccessPage from './pages/Login/KakaoSucess';
