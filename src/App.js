1c31fa3faf6f5b712f1aee16700343d7cacf216c
lucian1120000 <lucian1120@naver.com>
2025-01-31 12:10:23 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.jsx
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/main";
+import Main from "./pages/Main/Main";
 import Translation from "./pages/Translation/Translation";
 import NaverLogin from "./pages/Login/Naver";
 import MyPage from "./pages/User/MyPage";
--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -125,9 +125,8 @@ function Main() {
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    className={`dropdown-icon ${
-                      showSourceDropdown ? "clicked" : ""
-                    }`}
+                    className={`dropdown-icon ${showSourceDropdown ? "clicked" : ""
+                      }`}
                   >
                     <polyline points="6 9 12 15 18 9" />
                   </svg>
@@ -186,9 +185,8 @@ function Main() {
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    className={`dropdown-icon ${
-                      showTargetDropdown ? "clicked" : ""
-                    }`}
+                    className={`dropdown-icon ${showTargetDropdown ? "clicked" : ""
+                      }`}
                   >
                     <polyline points="6 9 12 15 18 9" />
                   </svg>
@@ -224,7 +222,31 @@ function Main() {
                 </ul>
               )}
             </div>
-            <div className="translation-output">{translatedText}</div>
+            <div className="translation-output">
+              {translatedText}
+              <div className="output-button">
+                <button className="output-edit">
+                  <svg
+                    fill="#000000"
+                    width="25px"
+                    height="25px"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22,7.24a1,1,0,0,0-.29-.71L17.47,2.29A1,1,0,0,0,16.76,2a1,1,0,0,0-.71.29L13.22,5.12h0L2.29,16.05a1,1,0,0,0-.29.71V21a1,1,0,0,0,1,1H7.24A1,1,0,0,0,8,21.71L18.87,10.78h0L21.71,8a1.19,1.19,0,0,0,.22-.33,1,1,0,0,0,0-.24.7.7,0,0,0,0-.14ZM6.83,20H4V17.17l9.93-9.93,2.83,2.83ZM18.17,8.66,15.34,5.83l1.42-1.41,2.82,2.82Z" />
+                  </svg>
+                </button>
+                <button className="output-copy">
+                  <svg fill="#000000"
+                    width="25px"
+                    height="25px"
+                    viewBox="0 0 1920 1920"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 1919.887h1467.88V452.008H0v1467.88ZM1354.965 564.922v1242.051H112.914V564.922h1242.051ZM1920 0v1467.992h-338.741v-113.027h225.827V112.914H565.035V338.74H452.008V0H1920ZM338.741 1016.93h790.397V904.016H338.74v112.914Zm0 451.062h790.397v-113.027H338.74v113.027Zm0-225.588h564.57v-112.913H338.74v112.913Z" fill-rule="evenodd" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+
           </div>
         </div>
       </main>
