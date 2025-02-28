85285a3d2b72686554d5ce2e7e19a62137b97d78
aripos1 <ihsjjang@gmail.com>
2025-02-28 13:35:51 +0900

    리액트 학원 마지막

--- src/pages/Login/Naver.jsx
+++ src/pages/Login/Naver.jsx
@@ -4,13 +4,12 @@ const NaverLogin = () => {
   console.log("네이버 리디렉션 URI:", process.env.REACT_APP_NAVER_REDIRECT_URI);
   const handleNaverLogin = () => {
     const clientId = process.env.REACT_APP_NAVER_CLIENT_ID; // 네이버 클라이언트 ID
-    const redirectUri = process.env.REACT_APP_NAVER_REDIRECT_URI; // 리디렉션 URI
+    // const redirectUri = process.env.REACT_APP_NAVER_REDIRECT_URI; // 리디렉션 URI
+    const redirectUri = "https://gorani.world/naver-success"; // 리디렉션 URI 하드코딩
     const authUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=1234`;
- 
     window.location.href = authUrl; // 네이버 로그인 페이지로 리디렉션
   };
 
-  
   return (
     <button className="login-button naver" onClick={handleNaverLogin}>
       <img className="logo" src="../../images/naver.png" alt="naver" />
